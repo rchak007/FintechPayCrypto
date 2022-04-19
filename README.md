5a85e032f4d0da8809341327cc58aa6fc694e459
Chakravarti Raghavan <rchak1@aol.com>
2022-04-18 22:37:32 -0700

    updates

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
-# FintechPayCrypto
-Instantly pay the fintech professionals with cryptocurrency
+## Fintech application to pay in crypto for employees
+
+![An image shows a wallet with bitcoin.](Images/19-4-challenge-image.png)
+
+### Background
+
+Built a new and disruptive platform called Fintech Finder. Fintech Finder is an application that clients can use to find fintech professionals from among a list of candidates, hire them, and pay them. 
+
+Integrated the Ethereum blockchain network into the application in order to enable customers to instantly pay the fintech professionals whom they hire with cryptocurrency.
+
+
+
+### Implementation
+
+#### Streamlit, Python 
+
+Used two Python files, both of which are contained in the main folder.
+
+The first file `fintech_finder.py` contains the code associated with the web interface of your application. The code included in this file is compatible with the Streamlit library. 
+
+The second file  `crypto_wallet.py` contains the Ethereum transaction functions. Import statement integrates the `crypto_wallet.py` Python script into the Fintech Finder interface program that is found in the `fintech_finder.py` file.
+
+Integrating these two files will allow to automate the tasks associated with generating a digital wallet if need be or loading payment wallet account from the mnemonic 12 work seed phrase , accessing Ethereum account balances, and signing and sending transactions via a Kovan Ethereum testnet blockchain.
+
+Specifically, you will assume the perspective of a Fintech Finder customer in order to do the following:
+
+* Generate a new Ethereum account instance by using the mnemonic seed phrase provided by Ganache/Metamask for Kovan testnet.
+
+* Fetch and display the account balance associated with the Ethereum account address.
+
+* Calculate the total value of an Ethereum transaction, including the gas estimate, that pays a Fintech Finder candidate for their work.
+
+* Digitally sign a transaction that pays a Fintech Finder candidate, and send this transaction to the Kovan testnet blockchain.
+
+* Review the transaction hash code associated with the validated blockchain transaction.
+
+The transactions and payments can be validated on Kovan Etherscan explorer.
+
+
+
+### Streamlit Web Interface
+
+Here we have Client Ethernet address and balance with some employees and their wages info.
+
+Employee who needs to get paid can be selected and their wages will be shown with number of hours they worked and need to get paid. Employee's Ethereum wallet address is also pre-loaded where they want to get paid to.
+
+There is a Send button - 
+
+### Pay employee in Ethereum
+
+![fintechInitialScreen](Images/fintechInitialScreen.png)
+
+
+
+
+
+![fintechPayment](Images/fintechPayment.png)
+
+
+
+### Validate Transaction Hash on Kovan Etherscan
+
+Once Send transaction is selected then the Ethereum amount will be transferred from Client address to Employees address and a transaction Hash is shown.
+
+This can easily verified on the Kovan Etherscan explorer.
+
+![fintechKovanPayment](Images/fintechKovanPayment.png)
+
+
+
+
+
+### Client Ethereum address
+
+Client can also easily check their Ethereum wallet address to see payments made etc.
+
+
+
+
+
+![fintechPaymentAddress](Images/fintechPaymentAddress.png)
---
+++ crypto_wallet.py
@@ -0,0 +1,139 @@
+# Cryptocurrency Wallet
+################################################################################
+
+# This file contains the Ethereum transaction functions that you have created throughout this module’s lessons. By using import statements, you will integrate this `crypto_wallet.py` Python script into the Fintech Finder interface program that is found in the `fintech_finder.py` file.
+
+################################################################################
+# Imports
+import os
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+from bip44 import Wallet
+from coincurve import PrivateKey
+from web3 import Account
+from web3 import middleware
+from web3.gas_strategies.time_based import medium_gas_price_strategy
+
+################################################################################
+# Wallet functionality
+
+def generate_account():
+    """Create a digital wallet and Ethereum account from a mnemonic seed phrase."""
+    # Fetch mnemonic from environment variable.
+    mnemonic = os.getenv("MNEMONIC")
+
+    # Create Wallet Object
+    wallet = Wallet(mnemonic)
+    Account.enable_unaudited_hdwallet_features()
+    account_mnem1 = Account.from_mnemonic(mnemonic, account_path="m/44'/60'/0'/0/0")
+    account_mnem2 = Account.from_mnemonic(mnemonic, account_path="m/44'/60'/0'/0/1")
+    account_mnem3 = Account.from_mnemonic(mnemonic, account_path="m/44'/60'/0'/0/2")
+    print("address 0 ", account_mnem1.address)
+    print("address 1 ", account_mnem2.address)
+    print("address 2 ", account_mnem3.address)
+    # print("address 0 private key ", account_mnem1.privateKey)
+    # print("address 1 private key", account_mnem2.privateKey)
+    # print("address 2 private key ", account_mnem3.privateKey)
+    account_mnem3_private = account_mnem3.privateKey
+    # Derive Ethereum Private Key
+    private, public = wallet.derive_account("eth")
+    # print ("private key in hex= ", private.hex())
+    # print ("Public key = ", public)
+    # sk = PrivateKey(private)
+    # print("type of private = ", type(private))
+    # print("bytes = ", (bytes.fromhex(private).decode('utf-8')))
+    # Convert private key into an Ethereum account
+    account = Account.privateKeyToAccount(private)
+
+    #return account , private
+    return account_mnem3, account_mnem3_private
+
+def get_balance(w1, address):
+    """Using an Ethereum account address access the balance of Ether"""
+    # Get balance of address in Wei
+    wei_balance = w1.eth.get_balance(address)
+
+    # Convert Wei value to ether
+    ether = w1.fromWei(wei_balance, "ether")
+
+    # Return the value in ether
+    return ether
+
+
+def send_transaction(w1, account, to, wage, privKey):
+    """Send an authorized transaction to the Ganache blockchain."""
+    # Set gas price strategy
+    w1.eth.setGasPriceStrategy(medium_gas_price_strategy)
+
+    # Convert eth amount to Wei
+    value = w1.toWei(wage, "ether")
+
+    # Calculate gas estimate
+    gasEstimate = w1.eth.estimateGas({"to": to, "from": account.address, "value": value})
+    n1 = w1.eth.get_transaction_count(account.address)
+    print("n1 = ", n1)
+    # Construct a raw transaction
+    raw_tx = {
+        "to": to,
+        "from": account.address,
+        "value": value,
+        "gas": gasEstimate,
+        "gasPrice": w1.toWei('150','gwei'),
+        "chainId": 42,  # Kovan
+        # "nonce": w1.eth.getTransactionCount(account.address)
+        "nonce": n1
+    }
+
+    # Sign the raw transaction with ethereum account
+    # print ("from inside send txn PrivKey in hex = " , privKey.hex())
+    #signed_tx = account.signTransaction(raw_tx)
+    print (" Raw txn = ", raw_tx)
+    try: 
+        signed_tx = w1.eth.account.sign_transaction(raw_tx, private_key=privKey)
+    except Exception as error_sign_txn:
+        print(" error signing = ", error_sign_txn)
+    # print(" Signed txn = " , signed_tx)
+    #signed_tx = account.sign_transaction(raw_tx)
+    block = w1.eth.get_block('latest')
+
+    # print ("Latest Block = ", block['number'])
+    # print ("Gas estimate = ", gasEstimate)
+#     sendTxn = w1.eth.send_transaction({
+#     'to': to,
+#     'from': account.address,
+#     'value': value,
+#     'gas': gasEstimate,
+#     'maxFeePerGas': w1.toWei(250, 'gwei'),
+#     'maxPriorityFeePerGas': w1.toWei(2, 'gwei'),
+# })
+
+
+    print (" to address = " , to)
+    print (" from address = " , account.address)
+    print (" value = ", value)
+    # print (" nonce = ", w1.eth.getTransactionCount(account.address))
+    #print (" nonce = ", w1.eth.get_transaction_count(account.address))
+
+
+
+    #print (" Signed Txn = ", signed_tx)
+    # Send the signed transactions
+    # sendTxn = w1.eth.sendRawTransaction(signed_tx.rawTransaction)
+    try:
+        sendTxn = w1.eth.send_raw_transaction(signed_tx.rawTransaction)
+    except Exception as error_sending_txn:
+        print (" exception sendTxn ", error_sending_txn)
+        error_send = 1
+    print (" Send Txn = ", sendTxn.hex())
+    # return w1.eth.sendRawTransaction(signed_tx.rawTransaction)
+
+    # print(w1.eth.get_transaction(sendTxn))
+    # print( w1.eth.get_transaction_receipt(sendTxn) )
+    # https://ethereum.stackexchange.com/questions/101513/send-eth-transaction-from-one-address-to-another-with-web3py
+    try: 
+        receipt1 = w1.eth.wait_for_transaction_receipt(sendTxn)
+    except Exception as error_receipt:
+        print (" Exception receipt = ". error_receipt)
+    
+    return sendTxn
